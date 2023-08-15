1333d71cfb6e365772fb44077dda9b474accd1a0
liu <2302563948@qq.com>
2023-08-15 09:13:02 +0800

    ✨feat: 添加SEO优化，ICP备案等

---
+++ xin-web/src/models/dictModel.ts
@@ -0,0 +1,63 @@
+// 全局共享数据示例
+import {useMemo, useState} from 'react';
+import {gitDict} from "@/services/system";
+
+interface DictItem {
+  label:string
+  value: string
+  type?: 'badge' | 'tag'
+  status?: 'success' | 'error' | 'default' | 'processing' | 'warning'
+}
+
+interface DictDate {
+  code: string
+  name: string
+  type?: number
+  dictItems: DictItem[]
+}
+
+const useUser = () => {
+
+  const [dictionaryCache , setDictionaryCache ] = useState<Map<string,DictItem[]>>(new Map())
+
+  /**
+   * 格式化字典
+   * @param data
+   */
+  const setDictJson = (data: DictDate[]): Map<string,DictItem[]> => {
+    let dictMap: Map<string,DictItem[]>= new Map()
+    data.forEach((dict: DictDate) =>{
+      dictMap.set(dict.code,dict.dictItems.map(a=>Object.assign(a,{type:dict.type})))
+    })
+    return dictMap
+  }
+
+  /**
+   * 通过键值获取字典
+   * @param key
+   */
+  const getDictionaryData = (key:string): DictItem[] => {
+    if(!dictionaryCache.size && localStorage.getItem('dictMap')){
+      console.log('-------获取字典缓存--------')
+      setDictionaryCache(setDictJson(JSON.parse(localStorage.getItem('dictMap')!)))
+    }
+    return dictionaryCache.has(key)?dictionaryCache.get(key)!:[]
+  }
+
+
+  useMemo (()=>{
+    gitDict().then(res=>{
+      let { success, data } = res
+      if(success){
+        localStorage.setItem('dictMap',JSON.stringify(data))
+        setDictionaryCache(setDictJson(data))
+      }
+    })
+  },[])
+
+  return {
+    getDictionaryData
+  };
+};
+
+export default useUser;
