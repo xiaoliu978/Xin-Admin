d69b6ad476af3ff092a489f73b362297fe4c551b
liu <2302563948@qq.com>
2023-11-07 09:09:44 +0800

    add: Xin Admin Taro 项目

---
+++ xin-taro/src/app.ts
@@ -0,0 +1,15 @@
+import { PropsWithChildren } from 'react'
+import { useLaunch } from '@tarojs/taro'
+import './app.less'
+
+function App({ children }: PropsWithChildren<any>) {
+
+  useLaunch(() => {
+    console.log('App launched.')
+  })
+
+  // children 是将要会渲染的页面
+  return children
+}
+
+export default App
---
+++ xin-taro/src/pages/index/index.tsx
@@ -0,0 +1,16 @@
+import { View, Text } from '@tarojs/components'
+import { useLoad } from '@tarojs/taro'
+import './index.less'
+
+export default function Index() {
+
+  useLoad(() => {
+    console.log('Page loaded.')
+  })
+
+  return (
+    <View className='index'>
+      <Text>SB</Text>
+    </View>
+  )
+}
