9590d96ba2f00893a67746c28ce2009639fd28da
liu <2302563948@qq.com>
2024-03-07 15:41:37 +0800

    update: 更新会员中心页面样式

--- xin-web/src/pages/frontend/User/components/UserLayout/index.tsx
+++ xin-web/src/pages/frontend/User/components/UserLayout/index.tsx
@@ -1,90 +1,64 @@
 import React from 'react';
 import { UserOutlined } from '@ant-design/icons';
 import type { MenuProps } from 'antd';
-import { Avatar, Layout, Menu, Space, theme } from 'antd';
-import headerImg from '@/assets/static/header.png';
+import { Avatar, Card, Col, Menu, Row, Space } from 'antd';
 import { useModel, useNavigate } from '@umijs/max';
 import { FormattedMessage } from '@@/exports';
+
 type MenuItem = Required<MenuProps>['items'][number];
 
-function getItemChildren(
-  label: React.ReactNode,
-  key: React.Key,
-  icon?: React.ReactNode,
-  children?: MenuItem[],
-  type?: 'group',
-): MenuItem {
-  return {
-    key,
-    icon,
-    children,
-    label,
-    type,
-  } as MenuItem;
-}
-
-const { Content, Sider } = Layout;
-
-
-const layoutContentCss: React.CSSProperties = {
-  minHeight: 200,
-  backgroundImage: 'url(' + headerImg + ')',
-  backgroundSize: '100% 100%',
-  backgroundRepeat: 'no-repeat',
-  backgroundPosition: 'center',
-  display: 'flex',
-  flexDirection: 'column-reverse',
-  color: '#fff',
-  fontWeight: 800,
-  letterSpacing: 2,
-};
-
-const headStyle = {
-  backgroundImage: 'url(data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAABQAAAABdCAMAAADNEjtLAAABEVBMVEUDAAADAAADAAADAAADAAADAAADAAADAAADAAADAAADAAADAAADAAADAAADAAADAAADAAADAAADAAADAAADAAADAAADAAADAAADAAADAAADAAADAAADAAADAAADAAADAAADAAADAAADAAADAAADAAADAAADAAADAAADAAADAAADAAADAAADAAADAAADAAADAAADAAADAAADAAADAAADAAADAAADAAADAAADAAADAAADAAADAAADAAADAAADAAADAAADAAADAAADAAADAAADAAADAAADAAADAAADAAADAAADAAADAAADAAADAAADAAADAAADAAADAAADAAADAAADAAADAAADAAADAAADAAADAAADAACnjvHuAAAAW3RSTlMBAgMEBQYHCAkKCwwNDxAREhQVFhgZGxweHyEiJCUnKSosLS8xMjQ2ODk7PT5AQkNFR0hKTE5PUVNUVldZW1xeX2FiZGVnaGprbG5vcHFzdHV2d3h5ent8fX5/XoZ4lgAAAPVJREFUeAHt1AERADAQAiDdrX/mDyKEIAAAjGkDsOk3AAIEECCAAAEECCBAAAECCBBAgAACBBAggAABBAggQAABAggQQIAAAgQQIIAAAQQIIEAAAQIIEECAAAIEECCAAAEECAgQQIAAAgQQIIAAAQQIIEAAAQIIEECAAAIEECCAAAEECCBAAAECCBBAgAACBBAggAABBAggQAABAggQQIAAAgQQIIAAAQECCBBAgAACBBAggAABBAggQAABAggQQIAAAgQQIIAAAQQIIEAAAQIIEECAAAIEECCAAAEECCBAAAECCBBAgAACBAQIIEAAAQIseAEYdZ9sAcJDX4ICAAAAAElFTkSuQmCC)',
-  width: '100%',
-  paddingBottom: 20,
-  paddingLeft: 20,
-}
-
-const getItem = (menus?: Menus[]): MenuItem[] => {
-  let data:  MenuItem[] | undefined = menus?.map((item) => {
-    if(item.children) {
-      let children: MenuItem[] = getItem(item.children)
-      return getItemChildren(item.locale?<FormattedMessage id={item.locale} />: item.name,item.path,'',children)
+const getItem = (menus?: USER.MenuType[]): MenuItem[] => {
+  let data: MenuItem[] | undefined = menus?.map((item) => {
+    if (item.children) {
+      let children: MenuItem[] = getItem(item.children);
+      return {
+        key: item.path,
+        children: children,
+        label: item.locale ? <FormattedMessage id={item.locale} /> : item.name
+      }
     }
-    return getItemChildren(item.locale?<FormattedMessage id={item.locale} />: item.name,item.path)
-  })
-  if(!data) return []
-  return data
-}
-
+    return {
+      key: item.path,
+      label: item.locale ? <FormattedMessage id={item.locale} /> : item.name
+    }
+  });
+  if (!data) return [];
+  return data;
+};
 
 export default (props: { children: React.ReactNode, selectedKey: string }) => {
   let navigate = useNavigate();
-  const { token: { colorBgContainer } } = theme.useToken();
   const { initialState } = useModel('@@initialState');
   return (
-    <Content style={{ maxWidth: 1460, width: '100%', margin: '0 auto' }}>
-      <Content style={layoutContentCss}>
-        <Space style={headStyle}>
-          <Avatar icon={<UserOutlined />} src={initialState!.currentUser!.avatar_url} />
-          {initialState!.currentUser!.nickname || initialState!.currentUser!.name || initialState!.currentUser!.username}
-        </Space>
-      </Content>
-      <Layout style={{ padding: '24px 0', background: colorBgContainer }}>
-        <Sider style={{ background: colorBgContainer }}>
+    <Row gutter={20} style={{ maxWidth: 1460, width: '100%', margin: '0 auto' }}>
+      <Col flex={'300px'}>
+        <Card style={{ marginTop: 20, marginBottom: 20 }}>
+          <Space>
+            <Avatar icon={<UserOutlined />} size={52} src={initialState!.currentUser!.avatar_url} />
+            <div>
+              <div>{initialState!.currentUser!.nickname || initialState!.currentUser!.name || initialState!.currentUser!.username}</div>
+              <div style={{ marginTop: '10px', color: '#8a919f' }}>一个热爱开发的同学</div>
+            </div>
+          </Space>
+        </Card>
+        <Card>
           <Menu
+            style={{ border: 'none' }}
             mode='inline'
             defaultSelectedKeys={['index']}
             items={getItem(initialState!.menus!.find((item) => item.key === 'user')!.children)}
             selectedKeys={[props.selectedKey]}
-            onSelect={({ key }) => {navigate(key);}}
+            onSelect={(data) => {
+              navigate(data.key);
+            }}
           />
-        </Sider>
-        <Content style={{ padding: '0 24px', minHeight: 280 }}>
+        </Card>
+      </Col>
+      <Col flex={'auto'}>
+        <Card style={{ marginTop: 20 }}>
           {props.children}
-        </Content>
-      </Layout>
-    </Content>
+        </Card>
+      </Col>
+    </Row>
   );
 }
--- xin-web/types/user.d.ts
+++ xin-web/types/user.d.ts
@@ -24,6 +24,7 @@ declare namespace USER {
     icon: string;
     children: MenuType[];
     key: string;
+    locale: string;
   }
 
 
